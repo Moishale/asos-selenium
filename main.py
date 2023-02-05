6cf9e85c3ca2e51c9275152b6897bf655bb5c49f
Moishale <99847759+Moishale@users.noreply.github.com>
2023-02-06 00:31:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from ClothoWeb.clothoweb import ClothoWeb
 
 with ClothoWeb() as bot:
     bot.land_first_page()
-    bot.select_product_to_search('t shirt')
+    bot.select_product_to_search(input('Enter Product'))
     bot.report_results()
     
